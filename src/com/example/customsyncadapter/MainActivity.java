c8e24280ee12523f64748b7375b30071eeab8629
Nelson <nelson88nica@gmail.com>
2013-04-20 12:04:14 -0600

    Ejemplo de retribucion de datos locales sin ContentProvider

--- src/com/example/customsyncadapter/MainActivity.java
+++ src/com/example/customsyncadapter/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.customsyncadapter;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+
+import com.example.customsyncadapter.db.DataSourceSinProvider;
 
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		DataSourceSinProvider dataSource = new DataSourceSinProvider (this);
+		dataSource.open();
+		Cursor cursor = dataSource.fetchPersonas();
+		
+		while(cursor.moveToNext()){
+			Log.i("Sample", cursor.getString(1));
+			Log.i("Sample", cursor.getString(2));
+		}
+		cursor.close();
+		dataSource.close();
+		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
---
+++ src/com/example/customsyncadapter/db/SimpleDatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.example.customsyncadapter.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SimpleDatabaseHelper extends SQLiteOpenHelper{
+
+	final static int VERSION = 1;
+	final static String DATABASENAME="simplesync";
+
+	
+	public SimpleDatabaseHelper(Context context) {
+		super(context, DATABASENAME, null, VERSION);
+		
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("create table personas ( _ID int primary key, nombre varchar(200) not null, apellido varchar(200) not null)");		
+		
+		//estos son datos de pruebas
+		db.execSQL("insert into personas (nombre, apellido) values('jose','ayerdis')");
+		db.execSQL("insert into personas (nombre, apellido) values('jose1','ayerdis1')");
+		db.execSQL("insert into personas (nombre, apellido) values('jose2','ayerdis2')");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		//nothing here move along
+	}
+
+}
