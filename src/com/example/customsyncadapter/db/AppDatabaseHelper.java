735e9dc2029b5d7bd7c9e4addfb3a506e93f10ca
Nelson <nelson88nica@gmail.com>
2013-04-26 14:24:17 -0600

    Renombrar SimpleDatabaseHelper a DatabaseHelper.

--- src/com/example/customsyncadapter/db/AppDatabaseHelper.java
+++ src/com/example/customsyncadapter/db/AppDatabaseHelper.java
@@ -5,25 +5,23 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class SimpleDatabaseHelper extends SQLiteOpenHelper{
+public class AppDatabaseHelper extends SQLiteOpenHelper{
 
 	final static int VERSION = 1;
 	final static String DATABASENAME="simplesync";
 
 	
-	public SimpleDatabaseHelper(Context context) {
+	public AppDatabaseHelper(Context context) {
 		super(context, DATABASENAME, null, VERSION);
 		
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db){
-		db.execSQL("create table personas ( _ID int primary key, nombre varchar(200) not null, apellido varchar(200) not null)");		
 		
-		//estos son datos de pruebas
-		db.execSQL("insert into personas (nombre, apellido) values('jose','ayerdis')");
-		db.execSQL("insert into personas (nombre, apellido) values('jose1','ayerdis1')");
-		db.execSQL("insert into personas (nombre, apellido) values('jose2','ayerdis2')");
+		db.execSQL("create table personas ( _ID integer primary key autoincrement, nombre varchar(200) not null, apellido varchar(200) not null)");		
+	
+	
 	}
 
 	@Override
